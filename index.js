2aa22577724b4b5e627512b93ea89a93fff93024
valentinachavez <valencha10@gmail.com>
2019-05-23 19:43:37 -0500

    Agregan las visitas

--- index.js
+++ index.js
@@ -13,22 +13,19 @@ var fs = require ('fs');
 var f=new Date();
 cad=f.getHours(); 
 
-var contador = {
-   paguno : 0,
-   pagdos : 0,
-    pagtres : 0,
+var visitas = {
+    general: [],
+    registro: []
   };
 
-  function archivoEscrito(){
-    
-
-    fs.writeFileSync('visitas.txt', 'Visitas del pagUno: ' + contador.paguno+ f+'\nVisitas de pagdos: ' + contador.pagdos+ f+'\nVisitas de pagTres: ' + contador.pagtres+''+ f, 'utf8');
-
-    fs.readFile('visitas.txt', 'utf8', function(err, data){
-      if(err) throw err;
-      console.log(data);
-    });
-}
+  fs.readFile(__dirname + "/visitas.txt", (err, data) => {
+    if (err) {
+      console.log("No hay archivo");
+    } else {
+      visitas = JSON.parse(data);
+      console.log("Si hay archivo");
+    }
+  });
 
 
 app.use(express.urlencoded({extended: true}));
@@ -37,66 +34,112 @@ app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
 
-//aqui se empiezan a configurar las rutas
+function registrarVisita(url) {
+    let f = new Date();
+    if (visitas.general.length > 0) {
+      let encontro = false;
+  
+      visitas.general.forEach((temp, index) => {
+        if (temp.url == url) {
+            temp.visitas++;
+          let v = temp.visitas;
+          encontro = true;
+  
+          let contenido = {
+            url: url,
+            visitas: v,
+            fecha: f.getDate() + "/" + f.getMonth() + "/" + f.getFullYear(),
+            hora: f.getHours() + ":" + f.getMinutes()
+          };
+  
+          visitas.registro.push(contenido);
+        }
+      });
+  
+      if (encontro == false) {
+        let contenido = {
+          url: url,
+          visitas: 1,
+          fecha: f.getDate() + "/" + f.getMonth() + "/" + f.getFullYear(),
+          hora: f.getHours() + ":" + f.getMinutes()
+        };
+        visitas.general.push(contenido);
+        visitas.registro.push(contenido);
+      }
+    } else {
+      let contenido = {
+        url: url,
+        visitas: 1,
+        fecha: f.getDate() + "/" + f.getMonth() + "/" + f.getFullYear(),
+        hora: f.getHours() + ":" + f.getMinutes()
+      };
+      visitas.general.push(contenido);
+      visitas.registro.push(contenido);
+    }
+    fs.writeFile("visitas.txt", JSON.stringify(visitas), "utf8", function() {});
+  }
+
 
 app.get('/', function(request, response){   
-    //el contexto siempre es un objeto
+   
     var contexto = {
         titulo: 'Pagina principal',
-        layout:false
+        layout:false,
+        
 
     }
+    registrarVisita("inicio");
     response.render('home', contexto);
 });
 app.get('/admin', function(request, response){   
-    //el contexto siempre es un objeto
+   
     var contexto = {
         titulo: 'Admin',
         descripcion:'Lorem ipsum dolor sit amet consectetur adipiscing elit hendrerit malesuada, magnis fames ridiculus laoreet sollicitudin ad enim duis arcu, ',
-        layout:false
+        layout:false,
+        visitas: visitas 
 
     }
     response.render('admin', contexto);
 });
 
 app.get('/paguno', function(request, response){   
-   
-    contador.paguno++;
-    //el contexto siempre es un objeto
+  
+
     var contexto = {
         titulo: 'Página Uno',
         descripcion: 'Lorem ipsum dolor sit amet consectetur adipiscing elit hendrerit malesuada, magnis fames ridiculus laoreet sollicitudin ad enim duis arcu, ',
         layout:false
 
     }
-    archivoEscrito();
+    registrarVisita("pagouno");
     response.render('paguno', contexto);
 });
 
 
 app.get('/pagdos', function(request, response){   
-    contador.pagdos++;
+    
     var contexto = {
         titulo: 'Página Dos',
         descripcion: 'Lorem ipsum dolor sit amet consectetur adipiscing elit hendrerit malesuada, magnis fames ridiculus laoreet sollicitudin ad enim duis arcu, ',
         layout:false
 
     }
-    archivoEscrito();
+    registrarVisita("pagodos");
     response.render('pagdos', contexto);
 });
 
 app.get('/pagtres', function(request, response){   
-    //el contexto siempre es un objeto
-    contador.pagtres++;
+
+    
     var contexto = {
         titulo: 'Página Tres',
         descripcion: 'Lorem ipsum dolor sit amet consectetur adipiscing elit hendrerit malesuada, magnis fames ridiculus laoreet sollicitudin ad enim duis arcu, ',
         layout:false
 
     }
-    archivoEscrito();
+    registrarVisita("pagotres");
     response.render('pagtres', contexto);
 });
-//aqui se le dice el puerto y las rutas
+
 app.listen(4000);
